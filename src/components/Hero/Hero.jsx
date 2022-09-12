45ea030a62045c8c626330edcb5aebfb631e7efa
Jaroslaw Charchula <charchula@gmail.com>
2022-09-12 21:33:33 +0200

    add new png to assets edit Hero component and css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Header from '../Header/Header';
 import './Hero.css';
+import hero_img from '../../assets/hero_image.png';
+import hero_img_back from '../../assets/hero_image_back.png';
+import Heart from '../../assets/heart.png';
+import Calories from '../../assets/calories.png';
 
 const Hero = () => {
   return (
@@ -49,7 +53,27 @@ const Hero = () => {
           <button className='btn'>Lern More</button>
         </div>
       </div>
-      <div className='hero__right-h'>right side</div>
+      <div className='hero__right-h'>
+        <button className='btn'>Join Now</button>
+        <div className='hero__heart-rate'>
+          <img src={Heart} alt='heart rate' />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* hero imgs */}
+        <img src={hero_img} alt='' className='hero__img' />
+        <img src={hero_img_back} alt='' className='hero__img-back' />
+
+        {/* calories */}
+        <div className='hero__calories'>
+          <img src={Calories} alt='' />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
