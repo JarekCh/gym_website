c7c168dd9b0779eee482f03bc5e71b10aa47f7b6
Jaroslaw Charchula <charchula@gmail.com>
2022-09-13 20:42:46 +0200

    add plansData.js with data edit app js and css(high res fix) add Plans component and css edit plans component , display data

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Plans.css';
+import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
+
+const Plans = () => {
+  return (
+    <div className='plans__container'>
+      <div className='programs__header' style={{ gap: '2rem' }}>
+        <span className='stroke-text'>ready to start</span>
+        <span>your journey</span>
+        <span className='stroke-text'>now with us</span>
+      </div>
+
+      {/* plans card */}
+      <div className='plans'>
+        {plansData.map((plan, i) => (
+          <div className='plan' key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            <div className='plan__features'>
+              {plan.features.map((feature, i) => (
+                <div className='plan__feature' key={i}>
+                  <img src={whiteTick} alt='' />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>
+            <div>
+              <span>See more benefits -></span>
+            </div>
+            <button className='btn'>Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
