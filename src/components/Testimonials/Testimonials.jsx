b524460b9e0413adee0d748d33163ad854c99af0
Jaroslaw Charchula <charchula@gmail.com>
2022-09-14 02:13:48 +0200

    add leftArrow.png in assets edit Testimonials component and css, add right site with logic

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Testimonials.css';
 import { testimonialsData } from '../../data/testimonialsData';
+import leftArrow from '../../assets/leftArrow.png';
+import rightArrow from '../../assets/rightArrow.png';
 
 const Testimonials = () => {
   const [selected, setSelected] = useState(0);
@@ -20,7 +22,32 @@ const Testimonials = () => {
           {` - ${testimonialsData[selected].status}`}
         </span>
       </div>
-      <div className='testimonials__right'></div>
+      <div className='testimonials__right'>
+        <div></div>
+        <div></div>
+        <img src={testimonialsData[selected].image} alt='' />
+
+        <div className='testimonials__arrows'>
+          <img
+            onClick={() => {
+              selected === 0
+                ? setSelected(tLenght - 1)
+                : setSelected((prev) => prev - 1);
+            }}
+            src={leftArrow}
+            alt=''
+          />
+          <img
+            onClick={() => {
+              selected === tLenght - 1
+                ? setSelected(0)
+                : setSelected((prev) => prev + 1);
+            }}
+            src={rightArrow}
+            alt=''
+          />
+        </div>
+      </div>
     </div>
   );
 };
